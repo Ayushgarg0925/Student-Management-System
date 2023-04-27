4fe4122f15dbec0d252500bbd840b4208aa4faba
Ayushgarg0925 <ayushgarg0925@gmail.com>
2023-04-27 22:31:29 +0530

    second file

--- SMS_APP/views.py
+++ SMS_APP/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from SMS_APP.models import *
 
@@ -6,53 +6,23 @@ def home(request):
     if request.method=='GET':
         return render(request,'SMS_APP/index.html')          
     elif request.method=='POST':
-        username=request.POST.get('i1','NA')
-        userpwd=request.POST.get('p1','NA')   
-        if username=='admin' and userpwd=='admin':
-            data=login.objects.all()
-            d1={'c':data}
-            return render(request,'SMS_APP/show_all.html', context=d1) 
-        else:
-            a=login.objects.get(ename=username,epassword=userpwd)
-            d1={
-                'a':username,
-                'c':a,
-            }
-            return render(request,'SMS_APP/show.html', context=d1)    
-        
-def edit_page(request,a):
-    if request.method == 'GET':
-        return render(request,'SMS_APP/edit.html')
-    elif request.method == 'POST':
-        if login.objects.filter(id=a).exists():
+        try:
             username=request.POST.get('i1','NA')
-            userage=int(request.POST.get('i2',0))
-            userno=request.POST.get('i3',0)
-            useraddress=request.POST.get('i4','NA')
-            userpwd=request.POST.get('i5','NA')
-            login.objects.filter(id=a).update(ename=username,eage=userage,eno=userno,eaddress=useraddress,epassword=userpwd)
-            return render(request,'SMS_APP/show.html')
-def delete_data(request,a):
-    login.objects.filter(id=a).delete()
-    return HttpResponse('<h1>data delete successfully!!!</h1><a href="http://127.0.0.1:8000">Back</a>')
-
-
-def showall_edit_page(request,a):
-    if request.method == 'GET':
-        return render(request,'SMS_APP/edit.html')
-    elif request.method == 'POST':
-        if login.objects.filter(id=a).exists():
-            username=request.POST.get('i1','NA')
-            userage=int(request.POST.get('i2',0))
-            userno=request.POST.get('i3',0)
-            useraddress=request.POST.get('i4','NA')
-            userpwd=request.POST.get('i5','NA')
-            login.objects.filter(id=a).update(ename=username,eage=userage,eno=userno,eaddress=useraddress,epassword=userpwd)
-            return render(request,'SMS_APP/show_all.html')
-
-
-
-
+            userpwd=request.POST.get('p1','NA')
+            if username=='admin' and userpwd=='admin':
+                data=login.objects.all()
+                d1={'c':data}
+                return render(request,'SMS_APP/show_all.html', context=d1) 
+            else:
+                a=login.objects.get(ename=username,epassword=userpwd)
+                d1={
+                    'a':username,
+                    'c':a,
+                }
+                return render(request,'SMS_APP/show.html', context=d1)    
+        except:
+            return HttpResponse('<h1>wrong Id or Password</h1><br><a href="http://127.0.0.1:8000">Back</a>')
+            
 def log_in(request):
     if request.method=='GET':
         return render(request,'SMS_APP/login.html')          
@@ -69,7 +39,44 @@ def log_in(request):
         i.eaddress=useraddress
         i.epassword=userpwd
         i.save()
-        return HttpResponse('<h1>data add successfully!!!</h1><a href="http://127.0.0.1:8000">Back</a>')
+        return HttpResponse('<h1>data add successfully!!!</h1><a href="http://127.0.0.1:8000">Back</a>')       
+     
+def edit_page(request,a):
+    if request.method == 'GET':
+        return render(request,'SMS_APP/edit.html')
+    elif request.method == 'POST':
+        if login.objects.filter(id=a).exists():
+            username=request.POST.get('i1','NA')
+            userage=int(request.POST.get('i2',0))
+            userno=request.POST.get('i3',0)
+            useraddress=request.POST.get('i4','NA')
+            userpwd=request.POST.get('i5','NA')
+            login.objects.filter(id=a).update(ename=username,eage=userage,eno=userno,eaddress=useraddress,epassword=userpwd)
+            return render(request,'SMS_APP/show.html')
+
+def delete_data(request,a):
+    login.objects.filter(id=a).delete()
+    return HttpResponse('<h1>data delete successfully!!!</h1><a href="http://127.0.0.1:8000">Back</a>')
+
+
+def showall_edit_page(request,a):
+    if request.method == 'GET':
+        sm=login.objects.get(id=a)
+        d1={'sm':sm}
+        return render(request,'SMS_APP/edit.html',context=d1)
+    elif request.method == 'POST':
+        if login.objects.filter(id=a).exists():
+            username=request.POST.get('i1','NA')
+            userage=int(request.POST.get('i2',0))
+            userno=request.POST.get('i3',0)
+            useraddress=request.POST.get('i4','NA')
+            userpwd=request.POST.get('i5','NA')
+            login.objects.filter(id=a).update(ename=username,eage=userage,eno=userno,eaddress=useraddress,epassword=userpwd)
+            # return render(request,'SMS_APP/index.html')
+            return redirect('http://127.0.0.1:8000')
 
 
 
+def delete_data_allpage(request,a):
+    login.objects.filter(id=a).delete()
+    return render(request,'SMS_APP/show_all.html')
